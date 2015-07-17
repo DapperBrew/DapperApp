47a6c47905b1dfe80a35bc704d3b73f0d0646c79
Dan Brown <slightlyoffbeat@gmail.com>
2015-07-17 00:39:12 -0700

    init
    scaffolding

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+
+// compile Sass
+gulp.task('sass', function() {
+	return gulp.src('app/scss/style.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		})
+	 );
+});
+
+// watch files
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
+	gulp.watch('app/*.html', ['copy']);
+});
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: 'dist'
+		}
+	})
+})
+
+//Copy necessary
+gulp.task( 'copy', function() {
+	// copy index.html to app
+	gulp.src('app/*.html')
+		.pipe( gulp.dest( 'dist' ));
+	// copy jquery to app
+	gulp.src('bower_components/jquery/dist/jquery.min.js')
+		.pipe( gulp.dest( 'dist/js/vendor' ));
+	// copy modernizr to app
+	gulp.src('bower_components/modernizr/modernizr.js')
+		.pipe( gulp.dest( 'dist/js/vendor' ))
+		.pipe(browserSync.reload({
+    	stream: true
+  	})
+	);
+});
+
+
+
+
+// default task
+gulp.task('default', ['watch'], function(){});
